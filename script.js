e63242c444c9757ba41d1a9e8952967896e0ee6c
Karoline Barros <karolbarros091@gmail.com>
2022-07-27 19:50:20 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function main()
+{
+    var resp = document.getElementById('resp');
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var texto = '';
+
+    if(document.getElementById('soma').checked)
+        texto = soma(num1, num2);
+    if(document.getElementById('menos').checked)
+        texto = menos(num1, num2);
+    if(document.getElementById('multiplicacao').checked)
+        texto = multiplicacao(num1, num2);
+    if(document.getElementById('divisao').checked)
+        texto = divisao(num1, num2);
+        
+        resp.innerHTML = texto;
+
+}
+
+function soma(x, y){
+    
+    return (x+y);
+}
+
+function menos(x, y){
+
+    return (x-y);
+}
+
+function divisao(x, y){
+
+    return (x/y);
+}
+
+function multiplicacao(x,y){
+     
+    return (x*y);
+}
